69b53e62f79ba36490dc3c0bb5d1feeaea3d2903
minggo <minggo@mmd-mac03deMac-mini.local>
2016-01-06 17:55:52 +0800

    commit category

--- testInputView/testInputView/NSAttributedString+Emotion.m
+++ testInputView/testInputView/NSAttributedString+Emotion.m
@@ -13,24 +13,18 @@
 
 
 -(NSString *) mgo_getPlainString {
-    //最终纯文本
-    NSMutableString *plainString = [NSMutableString stringWithString:self.string];
     
-    //替换下标的偏移量
-    __block NSUInteger base = 0;
+    NSMutableString *sourceString = [NSMutableString stringWithString:self.string];
     
-    //遍历
+    __block NSUInteger index = 0;
+    
     [self enumerateAttribute:NSAttachmentAttributeName inRange:NSMakeRange(0, self.length) options:0 usingBlock:^(id value, NSRange range, BOOL *stop) {
-                      
-        //检查类型是否是自定义NSTextAttachment类
         if (value && [value isKindOfClass:[EmotionTextAttachment class]]) {
-            //替换
-            [plainString replaceCharactersInRange:NSMakeRange(range.location + base, range.length) withString:((EmotionTextAttachment *) value).emotionStr];
-            //增加偏移量
-            base += ((EmotionTextAttachment *) value).emotionStr.length - 1;
+            [sourceString replaceCharactersInRange:NSMakeRange(range.location + index, range.length) withString:((EmotionTextAttachment *) value).emotionStr];
+            index += ((EmotionTextAttachment *) value).emotionStr.length - 1;
         }
     }];
     
-    return plainString;
+    return sourceString;
 }
 @end
